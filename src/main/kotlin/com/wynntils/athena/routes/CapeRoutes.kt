b861ac339b1dd054f23c38508ccc592b3b896c89
HeyZeer0 <21368488+HeyZeer0@users.noreply.github.com>
2020-06-17 18:38:44 -0300

    feat: ability to upload higher res capes

--- src/main/kotlin/com/wynntils/athena/routes/CapeRoutes.kt
+++ src/main/kotlin/com/wynntils/athena/routes/CapeRoutes.kt
@@ -141,8 +141,8 @@ class CapeRoutes {
                 fileResult["message"] = "The provided file is not a PNG image."
                 continue
             }
-            if (image.width != 128 || image.height != 64) {
-                fileResult["message"] = "The image needs to be 128x64."
+            if ((image.width + image.height) % 32 != 0 || (image.width != image.height * 2)) {
+                fileResult["message"] = "The image needs to be multiple of 64x32."
                 continue
             }
 
