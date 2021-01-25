e12329b56d48de0bf626af799ba3c7b4c266879f
P0ke <3767283+P0keDev@users.noreply.github.com>
2021-01-25 03:05:21 -0800

    Generate territority list solely from Wynn api

--- src/main/kotlin/com/wynntils/athena/routes/caches/TerritoryListCache.kt
+++ src/main/kotlin/com/wynntils/athena/routes/caches/TerritoryListCache.kt
@@ -7,7 +7,6 @@ import com.wynntils.athena.core.configs.apiConfig
 import com.wynntils.athena.core.configs.generalConfig
 import com.wynntils.athena.core.utils.JSONOrderedObject
 import com.wynntils.athena.routes.managers.GuildManager
-import org.json.simple.JSONArray
 import org.json.simple.JSONObject
 import java.net.URL
 import java.nio.charset.StandardCharsets
@@ -16,17 +15,10 @@ import java.nio.charset.StandardCharsets
 class TerritoryListCache: DataCache {
 
     /**
-     * Generates the cache based on merging Wynn and Scyu's Territory Data
+     * Generates the cache based on Wynn's Territory Data
      */
     override fun generateCache(): JSONOrderedObject {
-        var connection = URL(apiConfig.scyuTerritories).openConnection()
-        connection.setRequestProperty("User-Agent", generalConfig.userAgent)
-        connection.readTimeout = 5000
-        connection.connectTimeout = 5000
-
-        val scyuTerritories =  connection.getInputStream().readBytes().toString(StandardCharsets.UTF_8).asJSON<JSONArray>()
-
-        connection = URL(apiConfig.wynnTerritories).openConnection()
+        val connection = URL(apiConfig.wynnTerritories).openConnection()
         connection.setRequestProperty("User-Agent", generalConfig.userAgent)
         connection.setRequestProperty("apikey", apiConfig.wynnApiToken)
 
@@ -38,36 +30,22 @@ class TerritoryListCache: DataCache {
 
         val response = JSONOrderedObject()
         val result = response.getOrCreate<JSONOrderedObject>("territories")
-        for(territory in scyuTerritories) {
-            territory as JSONObject
+        for(name in wynnTerritories.keys) {
+            name as String
 
-            val name = territory["name"] as String
             val wynn = wynnTerritories[name] as JSONObject
-
             val final = result.getOrCreate<JSONOrderedObject>(name)
-
-            // location stuff
-            val start = territory["start"].toString().split(",")
-            val end = territory["end"].toString().split(",")
-
-            val location = final.getOrCreate<JSONOrderedObject>("location")
-            location["startX"] = start[0].toInt()
-            location["startZ"] = start[1].toInt()
-            location["endX"] = end[0].toInt()
-            location["endZ"] = end[1].toInt()
-            location["spawn"] = territory["spawnLocation"]
 
             val guild = GuildManager.getGuildData(wynn["guild"] as String)!!
 
-            // merging stuff
             final["territory"] = wynn["territory"]
             final["guild"] = guild.id
             final["guildPrefix"] = guild.prefix
             final["guildColor"] = guild.color
             final["acquired"] = wynn["acquired"]
             final["attacker"] = wynn["attacker"]
-            final["level"] = territory["level"]
-            final["location"] = location
+            final["level"] = 1 // not used
+            final["location"] = wynn["location"]
         }
 
         return response
